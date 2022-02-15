3d6ae94e941e0e1e7a21a985f2a1456ef0ccfcc0
Gulnaz11 <81852862+Gulnaz11@users.noreply.github.com>
2022-02-15 15:59:39 +0300

    Add  style
    Стилизовала компонент Message

--- src/components/Message/index.jsx
+++ src/components/Message/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import style from './message.module.css';
 
 export const Message = ({content}) => {
     return (
-        <div>
-            {content}
+        <div className={style.message}>
+            <h1>Message:</h1>
+            <p className={style.messageContent}>{content}</p>
         </div>
     );
 };
