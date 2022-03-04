e7dbfe57f92440d5468f3ffab9ad708ef26c7e98
Gulnaz11 <81852862+Gulnaz11@users.noreply.github.com>
2022-03-04 18:38:38 +0300

    Use Materual UI
    Установила material-ui. Передела форму с использованием компонентов из material-ui

--- src/App.js
+++ src/App.js
@@ -1,4 +1,15 @@
-import './App.css';
+import {
+    Container,
+    Button,
+    TextField,
+    Input,
+    Box,
+    ListItem,
+    ListItemText,
+    ListItemButton,
+    ListItemIcon, IconButton, Checkbox, Select, MenuItem
+} from '@mui/material'
+import Paper from '@mui/material/Paper';
 import {useState, useCallback, useMemo, useEffect} from "react";
 import {useCreateMessageForm} from "./hooks/useCreateMessageForm";
 import {useDidUpdate} from "./hooks/useDidUpdate";
@@ -12,39 +23,67 @@ function App() {
     const {messageList,setMessageList, addNewMessage} = useMessageList();
 
     //  const {addNewTask, taskList,changeStatus,setTaskList}= useTaskList();
-  //
-   useCacheMessageList(messageList,setMessageList);
-  //
-  //
-  //  const {filterStatus,onChangeStatus} = useFilterByStatus();
-   const {
+    //
+    useCacheMessageList(messageList,setMessageList);
+    //
+    //
+    //  const {filterStatus,onChangeStatus} = useFilterByStatus();
+    const {
         handleSubmit,
         onChangeInput,
         inputValue
-   }=useCreateMessageForm ({onSubmit: addNewMessage});
+    }=useCreateMessageForm ({onSubmit: addNewMessage});
 
     useAddBotText(messageList,addNewMessage);
 
-  //  const {filteredTaskList} =useTaskFilteredByStatus({list:taskList,filterStatus})
+    //  const {filteredTaskList} =useTaskFilteredByStatus({list:taskList,filterStatus})
 
-  return (
-      <div className="App">
-          <form onSubmit={handleSubmit}>
-              <input onChange={onChangeInput} value={inputValue} type="text"/>
-              <button type="submit">send</button>
-          </form>
-          {
-              messageList.map(({text, author},index) => {
-                  return <div className="dialog" key={index}>
-                      <h4>{author}:</h4>
-                      <p>
-                      {text}
-                      </p>
-                  </div>
-                  })
-          }
-      </div>
-  );
-}
+    return (
+        <Container
+            sx={{
+                display: "flex",
+                alignItems: 'center',
+                height: '100hv'}}
+            maxWidth="xs"
+        >
+            <Paper
+                sx={
+                    {
+                        padding: 2,
+                        height: "80vh",
+                        width: '100%'
+                    }
+                }
+                elevation={3}
+            >
+                <Box component={"form"} onSubmit={handleSubmit}>
+                    <Input
+                        label="Enter text"
+                        placeholder="Hello"
+                        onChange={onChangeInput}
+                        value={inputValue}
+                        type="text"/>
+                    <Button variant="contained" type="submit">
+                        Send
+                    </Button>
+                </Box>
+                {
+                    messageList.map(({text, author},index) => {
+                        return  <ListItem key={index}>
+                            <ListItemText
+                                primary={text}
+                                secondary={author}
+                                secondaryTypographyProps={{
+                                    color:  author == "Me" ? 'rgb(187, 242, 194);' : 'rgb(242, 187, 218);',
+
+                                }}/>
+                        </ListItem>
+
+                    })
+                }
+            </Paper>
+        </Container>
+            );
+            }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CssBaseline from '@mui/material/CssBaseline';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createTheme, ThemeProvider} from "@mui/material";
+
+import App from './App';
+
+const theme = createTheme({
+    palette: {
+        type: 'light',
+        primary: {
+            main: '#9e318f',
+        },
+        secondary: {
+            main: '#f50057',
+        },
+        background: {
+            default: '#f9eeee',
+        },
+    },
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <CssBaseline/>
+            <App />
+        </ThemeProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
